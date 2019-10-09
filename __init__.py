88df3c4d52b810ff84ad2275ad7be2239ff15441
Inkognita <liepieshov@ucu.edu.ua>
2019-10-09 18:17:15 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,130 @@
+import collections
+import datetime
+import os
+import time
+from logging import Logger
+from functools import lru_cache
+
+import numpy as np
+import tensorflow as tf
+
+from . import model
+from .eval import resize_image, detect, sort_poly
+from .image_utils import four_point_transform, order_points
+
+logger = Logger(__name__)
+checkpoint_path = 'east_icdar2015_resnet_v1_50_rbox'
+
+
+@lru_cache(maxsize=100)
+def get_predictor(checkpoint_path):
+    input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
+    global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+
+    f_score, f_geometry = model.model(input_images, is_training=False)
+
+    variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
+    saver = tf.train.Saver(variable_averages.variables_to_restore())
+
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+
+    ckpt_state = tf.train.get_checkpoint_state(checkpoint_path)
+    model_path = os.path.join(checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
+    logger.info('Restore from {}'.format(model_path))
+    saver.restore(sess, model_path)
+
+    def predictor(img):
+        """
+        :return: {
+            'text_lines': [
+                {
+                    'score': ,
+                    'x0': ,
+                    'y0': ,
+                    'x1': ,
+                    ...
+                    'y3': ,
+                }
+            ],
+            'rtparams': {  # runtime parameters
+                'image_size': ,
+                'working_size': ,
+            },
+            'timing': {
+                'net': ,
+                'restore': ,
+                'nms': ,
+                'cpuinfo': ,
+                'meminfo': ,
+                'uptime': ,
+            }
+        }
+        """
+        start_time = time.time()
+        rtparams = collections.OrderedDict()
+        rtparams['start_time'] = datetime.datetime.now().isoformat()
+        rtparams['image_size'] = '{}x{}'.format(img.shape[1], img.shape[0])
+        timer = collections.OrderedDict([
+            ('net', 0),
+            ('restore', 0),
+            ('nms', 0)
+        ])
+
+        im_resized, (ratio_h, ratio_w) = resize_image(img)
+        rtparams['working_size'] = '{}x{}'.format(
+            im_resized.shape[1], im_resized.shape[0])
+        start = time.time()
+        score, geometry = sess.run(
+            [f_score, f_geometry],
+            feed_dict={input_images: [im_resized[:, :, ::-1]]})
+        timer['net'] = time.time() - start
+
+        boxes, timer = detect(score_map=score, geo_map=geometry, timer=timer)
+        logger.info('net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
+            timer['net'] * 1000, timer['restore'] * 1000, timer['nms'] * 1000))
+
+        if boxes is not None:
+            scores = boxes[:, 8].reshape(-1)
+            boxes = boxes[:, :8].reshape((-1, 4, 2))
+            boxes[:, :, 0] /= ratio_w
+            boxes[:, :, 1] /= ratio_h
+
+        duration = time.time() - start_time
+        timer['overall'] = duration
+        logger.info('[timing] {}'.format(duration))
+
+        text_lines = []
+        if boxes is not None:
+            text_lines = []
+            for box, score in zip(boxes, scores):
+                box = sort_poly(box.astype(np.int32))
+                if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+                    continue
+                tl = collections.OrderedDict(zip(
+                    ['x0', 'y0', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3'],
+                    map(float, box.flatten())))
+                tl['score'] = float(score)
+                text_lines.append(tl)
+            # 'rtparams': rtparams,
+            # 'timing': timer,
+        # ret.update(get_host_info())
+        return text_lines
+
+    return predictor
+
+
+def cut_boxes(img, bboxs):
+    for bbox in bboxs:
+        bbox = order_points(np.array([bbox['x0'], bbox['y0'], bbox['x1'], bbox['y1'], bbox['x2'],
+                                      bbox['y2'], bbox['x3'], bbox['y3']], dtype='int32').reshape((4, 2)))
+        patch = four_point_transform(img, bbox)
+        # transformed = transform(dict(image=patch))
+        # transformed["image"] = transformed["image"].view(1, *transformed["image"].size())
+        yield patch
+
+
+if __name__ == '__main__':
+    import cv2
+
+    img = cv2.imread("examples/1.jpg")
+    get_predictor(checkpoint_path)(img)
